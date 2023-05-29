69b29dcb20e03d881b0f74f6bc6678fde64c32c9
Marc Vilella <mvilella.and@gmail.com>
2023-05-29 17:45:34 +0200

    refactor: Set button internals with computed properties
    Improve code legibility

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,27 +1,40 @@
 <template>
   <button
-    :class="['button', { primary, alert, outline, flat, loading, block }]"
-    :style="`--width:${props.size}${isNumber(props.size) ? 'rem' : ''}`"
-    :type="props.submit ? 'submit' : props.reset ? 'reset' : 'button'"
+    :class="['button', modifiers]"
+    :style="style"
+    :type="type"
     :disabled="props.disabled">
     <slot />
   </button>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import { isNumber } from '/@/utils/number';
+
 const props = defineProps<{
+  // Modifiers
   primary?: boolean;
   alert?: boolean;
   outline?: boolean;
   flat?: boolean;
   block?: boolean;
   loading?: boolean;
+  // State
   disabled?: boolean;
+  // Button type shorthands
   submit?: boolean;
   reset?: boolean;
+  // Size
   size?: number | string;
 }>();
+
+const type = computed(() => props.submit ? 'submit' : props.reset ? 'reset' : 'button');
+const style = computed(() => `--width:${props.size}${isNumber(props.size) ? 'rem' : ''}`);
+const modifiers = computed(() => {
+  const { primary, alert, outline, flat, loading, block } = props;
+  return { primary, alert, outline, flat, loading, block };
+});
 </script>
 
 <style lang="scss" scoped>
