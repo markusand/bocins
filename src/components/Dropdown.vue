3aad218907dafedcf842762af543a6c5a72ffa79
Marc Vilella <mvilella.and@gmail.com>
2026-07-05 13:22:27 +0200

    refactor(Dropdown): dramatically simplify css positioning
    Rename to more semantic positions

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="togglerClasses" :style>
+  <div :class="togglerClasses">
     <slot name="toggler" :open :close>
       <button type="button" :popovertarget="id" :disabled tabindex="0">
         <slot name="label" :open :close>
@@ -14,8 +14,8 @@
     v-if="!disabled"
     :id
     ref="dropdown"
-    :class="dropdownClasses"
-    :style="toWidth(width)"
+    class="dropdown"
+    :style
     popover
     v-bind="$attrs"
     @toggle="onToggle">
@@ -29,10 +29,10 @@ import Icon from './Icon.vue';
 import { toWidth } from '/@/utils';
 
 export type DropdownPosition =
-  | 'top-left' | 'top-in-left' | 'top' | 'top-in-right' | 'top-right'
-  | 'right-in-top' | 'right' | 'right-in-bottom'
-  | 'bottom-right' | 'bottom-in-right' | 'bottom' | 'bottom-in-left' | 'bottom-left'
-  | 'left-in-bottom' | 'left' | 'left-in-top';
+  | 'top-left' | 'top-start' | 'top' | 'top-end' | 'top-right'
+  | 'left' | 'left-start' | 'left-end'
+  | 'right' | 'right-start' | 'right-end'
+  | 'bottom-left' | 'bottom-start' | 'bottom' | 'bottom-end' | 'bottom-right';
 
 export type DropdownProps = {
   icon?: string;
@@ -44,11 +44,30 @@ export type DropdownProps = {
   position?: DropdownPosition;
 };
 
+const POSITIONS: Record<DropdownPosition, string> = {
+  'top-left': 'block-start inline-start',
+  'top-right': 'block-start inline-end',
+  'bottom-start': 'block-end span-inline-end',
+  'bottom': 'block-end span-all',
+  'bottom-end': 'block-end span-inline-start',
+  'bottom-left': 'block-end inline-start',
+  'bottom-right': 'block-end inline-end',
+  'top-start': 'block-start span-inline-end',
+  'top': 'block-start span-all',
+  'top-end': 'block-start span-inline-start',
+  'right-start': 'inline-end span-block-end',
+  'right': 'inline-end span-all',
+  'right-end': 'inline-end span-block-start',
+  'left-start': 'inline-start span-block-end',
+  'left': 'inline-start span-all',
+  'left-end': 'inline-start span-block-start',
+};
+
 defineOptions({ inheritAttrs: false });
 
 const props = withDefaults(defineProps<DropdownProps>(), {
   label: '',
-  position: 'bottom-in-left',
+  position: 'bottom',
 });
 
 defineSlots<{
@@ -77,19 +96,12 @@ const togglerClasses = computed(() => ['toggler', {
   'is-open': isOpen.value,
 }]);
 
-const dropdownClasses = computed(() => ['dropdown', `dropdown--${props.position}`]);
-
-const CHEVRON_ROTATIONS: Record<string, string> = {
-  top: '180deg',
-  right: '-90deg',
-  left: '90deg',
-  bottom: '0deg',
-};
-
-const style = computed(() => {
-  const direction = props.position?.split('-')[0] ?? 'bottom';
-  return { '--chevron-rotate': CHEVRON_ROTATIONS[direction] ?? '0deg' };
-});
+const style = computed(() => ({
+  ...toWidth(props.width),
+  '--position': POSITIONS[props.position],
+  '--gap-x': /left|right/.test(props.position) ? 'var(--gap, 0)' : 0,
+  '--gap-y': /top|bottom/.test(props.position) ? 'var(--gap, 0)' : 0,
+}));
 
 const onToggle = (event: ToggleEvent) => {
   isOpen.value = event.newState === 'open';
@@ -101,118 +113,37 @@ const onToggle = (event: ToggleEvent) => {
 
 <style scoped>
 .dropdown {
-  --gap: var(--dropdown-margin, 0.125rem);
-  --translate: var(--dropdown-translate, 0 0.125rem);
+  --gap: var(--dropdown-gap, 0.25rem);
+  --timing: var(--dropdown-timing, 0.2s);
 
-  all: unset;
-  display: none;
-  position: absolute;
+  border: none;
+  background: none;
+  padding: 0;
+  inset: auto;
+  position: fixed;
   position-anchor: v-bind(anchor); /* stylelint-disable-line */
-  inset: var(--position);
-  position-try-fallbacks: var(--fallback);
-  translate: var(--translate);
+  position-area: var(--position, bottom);
+  position-try-fallbacks: flip-block, flip-inline, flip-block flip-inline;
+  min-inline-size: anchor-size(inline);
 
-  &:popover-open { display: block; }
+  /* Transition */
+  display: none;
+  margin: 0;
+  opacity: 0;
+  transition:
+    all var(--timing) linear,
+    display var(--timing) allow-discrete,
+    overlay var(--timing) allow-discrete;
 
-  /* Corners */
-  &.dropdown--top-left {
-    --position: auto anchor(left) anchor(top) auto;
-    --fallback: --bottom-left, --top-right, --bottom-right;
-  }
-
-  &.dropdown--top-right {
-    --position: auto auto anchor(top) anchor(right);
-    --fallback: --bottom-right, --top-left, --bottom-left;
-  }
-
-  &.dropdown--bottom-right {
-    --position: anchor(bottom) auto auto anchor(right);
-    --fallback: --top-right, --bottom-left, --top-left;
-  }
-
-  &.dropdown--bottom-left {
-    --position: anchor(bottom) anchor(left) auto auto;
-    --fallback: --top-left, --bottom-right, --top-right;
-  }
-
-  /* Top family */
-  &.dropdown--top-in-left {
-    --position: auto auto anchor(top) anchor(left);
-    --margin: var(--gap) 0;
-    --fallback: --bottom-in-left, --top-in-right, --bottom-in-right;
-  }
-
-  &.dropdown--top {
-    --translate: -50% 0;
-    --position: auto auto anchor(top) anchor(center);
-    --margin: var(--gap) 0;
-    --fallback: --bottom, --top-in-left, --bottom-in-left;
-  }
-
-  &.dropdown--top-in-right {
-    --position: auto anchor(right) anchor(top) auto;
-    --margin: var(--gap) 0;
-    --fallback: --bottom-in-right, --top-in-left, --bottom-in-left;
-  }
-
-  /* Right family */
-  &.dropdown--right-in-top {
-    --position: anchor(top) auto auto anchor(right);
-    --margin: 0 var(--gap);
-    --fallback: --left-in-top, --right-in-bottom, --left-in-bottom;
-  }
-
-  &.dropdown--right {
-    --translate: 0 -50%;
-    --position: anchor(center) auto auto anchor(right);
-    --margin: 0 var(--gap);
-    --fallback: --left, --right-in-top, --left-in-top;
-  }
-
-  &.dropdown--right-in-bottom {
-    --position: auto auto anchor(bottom) anchor(right);
-    --margin: 0 var(--gap);
-    --fallback: --left-in-bottom, --right-in-top, --left-in-top;
-  }
-
-  /* Bottom family */
-  &.dropdown--bottom-in-right {
-    --position: anchor(bottom) anchor(right) auto auto;
-    --margin: var(--gap) 0;
-    --fallback: --top-in-right, --bottom-in-left, --top-in-left;
-  }
-
-  &.dropdown--bottom {
-    --translate: -50% 0;
-    --position: anchor(bottom) auto auto anchor(center);
-    --margin: var(--gap) 0;
-    --fallback: --top, --bottom-in-left, --top-in-left;
-  }
-
-  &.dropdown--bottom-in-left {
-    --position: anchor(bottom) auto auto anchor(left);
-    --margin: var(--gap) 0;
-    --fallback: --top-in-left, --bottom-in-right, --top-in-right;
-  }
-
-  /* Left family */
-  &.dropdown--left-in-bottom {
-    --position: auto anchor(left) anchor(bottom) auto;
-    --margin: 0 var(--gap);
-    --fallback: --right-in-bottom, --left-in-top, --right-in-top;
-  }
-
-  &.dropdown--left {
-    --translate: 0 -50%;
-    --position: anchor(center) anchor(left) auto auto;
-    --margin: 0 var(--gap);
-    --fallback: --right, --left-in-top, --right-in-top;
-  }
-
-  &.dropdown--left-in-top {
-    --position: anchor(top) anchor(left) auto auto;
-    --margin: 0 var(--gap);
-    --fallback: --right-in-top, --left-in-bottom, --right-in-bottom;
+  &:popover-open {
+    display: block;
+    opacity: 1;
+    margin: var(--gap-y) var(--gap-x);
+    
+    @starting-style {
+      opacity: 0;
+      margin: 0;
+    }
   }
 }
 
@@ -231,97 +162,5 @@ const onToggle = (event: ToggleEvent) => {
 
     &:focus-visible { background: #8881; }
   }
-
-  .chevron {
-    rotate: var(--chevron-rotate, 0deg);
-    transition: rotate 0.2s;
-  }
-
-  &.is-open .chevron { rotate: calc(var(--chevron-rotate, 0deg) + 180deg); }
-}
-
-/* Corners */
-@position-try --top-left {
-  inset: auto anchor(left) anchor(top) auto;
-  margin: var(--gap);
-}
-
-@position-try --top-right {
-  inset: auto auto anchor(top) anchor(right);
-  margin: var(--gap);
-}
-
-@position-try --bottom-right {
-  inset: anchor(bottom) auto auto anchor(right);
-  margin: var(--gap);
-}
-
-@position-try --bottom-left {
-  inset: anchor(bottom) anchor(left) auto auto;
-  margin: var(--gap);
-}
-
-/* Top family */
-@position-try --top-in-left {
-  inset: auto auto anchor(top) anchor(left);
-  margin: var(--gap) 0;
-}
-
-@position-try --top {
-  inset: auto auto anchor(top) anchor(center);
-  margin: var(--gap) 0;
-}
-
-@position-try --top-in-right {
-  inset: auto anchor(right) anchor(top) auto;
-  margin: var(--gap) 0;
-}
-
-/* Right family */
-@position-try --right-in-top {
-  inset: anchor(top) auto auto anchor(right);
-  margin: 0 var(--gap);
-}
-
-@position-try --right {
-  inset: anchor(center) auto auto anchor(right);
-  margin: 0 var(--gap);
-}
-
-@position-try --right-in-bottom {
-  inset: auto auto anchor(bottom) anchor(right);
-  margin: 0 var(--gap);
-}
-
-/* Bottom family */
-@position-try --bottom-in-right {
-  inset: anchor(bottom) anchor(right) auto auto;
-  margin: var(--gap) 0;
-}
-
-@position-try --bottom {
-  inset: anchor(bottom) auto auto anchor(center);
-  margin: var(--gap) 0;
-}
-
-@position-try --bottom-in-left {
-  inset: anchor(bottom) auto auto anchor(left);
-  margin: var(--gap) 0;
-}
-
-/* Left family */
-@position-try --left-in-bottom {
-  inset: auto anchor(left) anchor(bottom) auto;
-  margin: 0 var(--gap);
-}
-
-@position-try --left {
-  inset: anchor(center) anchor(left) auto auto;
-  margin: 0 var(--gap);
-}
-
-@position-try --left-in-top {
-  inset: anchor(top) anchor(left) auto auto;
-  margin: 0 var(--gap);
 }
 </style>
